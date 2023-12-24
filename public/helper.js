49847591dcb20b0d65a4e2dfb2ef923952661ad4
xetadeveloper <jazelogin@hotmail.com>
2023-12-24 07:49:29 +0100

    fix: wrong import in helper file

--- public/helper.js
+++ public/helper.js
@@ -1,4 +1,4 @@
-import { socket } from './socket-io-config.js/index.js';
+import { socket } from './socket-io-config.js';
 
 const msgBoxHolder = document.querySelector('.msg-box-holder');
 const sideBar = document.querySelector('.side-bar');
